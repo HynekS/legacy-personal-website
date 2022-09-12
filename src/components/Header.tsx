6bace51ede293da8f45096c3e5aa557f60eb3f8f
HynekS <hyneksvacha@seznam.cz>
2022-09-12 20:10:38 +0200

    Update markup, add 'about' link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,17 @@ const Header = (): JSX.Element => {
     <header
       tw="top-0 flex justify-between pr-6 pl-4 lg:(pl-[2.25rem] sticky) pt-6 pb-4 w-full h-[4.5rem]" /* added explicit height and width to fight the layout shift*/
     >
-      <Link href="/" passHref>
-        <a aria-label="Link to homepage">
-          <Logo />
-        </a>
-      </Link>
-      <Link href="/">blog</Link>
-      <Link href="/work">work</Link>
+      <div tw="flex items-center gap-4">
+        <Link href="/" passHref>
+          <a aria-label="Link to homepage">
+            <Logo />
+          </a>
+        </Link>
+        <Link href="/">blog</Link>
+        <Link href="/work">projects</Link>
+        <Link href="/about">about</Link>
+      </div>
+
       <div>
         <ThemeToggle />
       </div>
